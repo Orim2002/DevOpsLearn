3785365b95f721b480efa9bd6501ca537cf6b7a0
orim2002 <o2002mm@gmail.com>
2026-01-19 23:34:45 +0200

    Add SecScan

--- main.tf
+++ main.tf
@@ -31,3 +31,12 @@ resource "docker_container" "nginx_srv" {
     external = var.external_port
   }
 }
+
+# resource "aws_s3_bucket_public_access_block" "state_security" {
+#   bucket = "orima-bucket"
+
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
