8ba8604a50874565b5288156e663d1422adf8c6c
orim2002 <o2002mm@gmail.com>
2026-01-20 13:25:29 +0200

    Add RDS

--- main.tf
+++ main.tf
@@ -163,11 +163,13 @@ resource "aws_security_group" "rds_sg" {
     security_groups = [aws_security_group.ecs_sg.id]
   }
 
+  #trivy:ignore:AVD-AWS-0104
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow only essential outbound traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
 
