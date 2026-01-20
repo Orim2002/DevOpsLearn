2f12004dfedb835f4a4d8791b141227fad86b3d2
orim2002 <o2002mm@gmail.com>
2026-01-20 11:58:17 +0200

    Fix Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "container_id" {
-  value = docker_container.nginx_srv.id
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
 }
 
-output "web_url" {
-  value = "http://localhost:${var.external_port}"
+output "ecs_task_arn" {
+  value = aws_ecs_task_definition.app_task.arn
+}
+
+output "security_group_id" {
+  value = aws_security_group.ecs_sg.id
 }
